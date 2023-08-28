390f495b00428afb02686fb1dc589a4eb538c927
ttajohnson <tim.ttaj@gmail.com>
2023-08-27 19:45:55 -0700

    Login Screen, App Customization

--- kivy_basics/customize_app.py
+++ kivy_basics/customize_app.py
@@ -1,15 +1,15 @@
 from kivy.app import App # Import the App class.
 from kivy.uix.gridlayout import GridLayout # Import GridLayout class, used as base for Root Widget.
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label # Import Label class for displaying text.
+from kivy.uix.textinput import TextInput # Import the TextInput class for...text input.
 
-class LoginScreen(GridLayout):
+class LoginScreen(GridLayout): # Our Root Widget
 
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs) # Overriding the __init__() method to add widgets and define their behaviors.
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
+        self.cols = 2 # Creating 2 columns.
+        self.add_widget(Label(text='User Name')) # Creating one widget for a User Name input in Column 1.
+        self.username = TextInput(multiline=False) # Creating a one-line text input.
         self.add_widget(self.username)
         self.add_widget(Label(text='Password'))
         self.password = TextInput(password = True, multiline=False)
--- kivy_basics/notes.md
+++ kivy_basics/notes.md
@@ -7,4 +7,6 @@
 
 ### Customizing an Application <customize_app.py>
     - You can set the title and icon of your application in the build() method; icons must be in main file directory.
-    - 
+
+### A Note on calling super().
+    - super() provides a way to access parent class methods that is more maintainable and flexible, especially when dealing with multiple inheritance. It allows you to avoid hard-coding parent class names in your methods and ensures that, in complex inheritance scenarios, each method is called exactly once.
